e55bc356755da29ae72f0f9f5189440eb8f34591
RahimIslam293 <110697424+RahimIslam293@users.noreply.github.com>
2025-04-01 07:52:45 -0400

    Made adjustments to align with solution.  My original solution worked but made a few tweaks

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ alfabet_df = pandas.read_csv("nato_phonetic_alphabet.csv") #import csv
 #print(alfabet_df)
 alfabet_df_dict = {row.letter : row.code for (index,row) in alfabet_df.iterrows()}
 
-name = input("Please provide your name: ")
+name = input("Please provide your name: ").upper()
 #name = "test"
 name_letters = list(name.upper())
-name_alfa = [alfabet_df_dict[letter] for letter in name_letters if letter in alfabet_df_dict]
+name_alfa = [alfabet_df_dict[letter] for letter in name]
 
 print("Your translation is: ")
 print(name_alfa)
