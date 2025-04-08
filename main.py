9c3fbd3ba5499a691368022e56032624cb0b69a6
RahimIslam293 <110697424+RahimIslam293@users.noreply.github.com>
2025-04-08 07:26:02 -0400

    Added additional checks on checking for numbers

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ alfabet_df = pandas.read_csv("nato_phonetic_alphabet.csv") #import csv
 #print(alfabet_df)
 alfabet_df_dict = {row.letter : row.code for (index,row) in alfabet_df.iterrows()}
 
-name = input("Please provide your name: ").upper()
-#name = "test"
-name_letters = list(name.upper())
-name_alfa = [alfabet_df_dict[letter] for letter in name]
+def generate_phonetic():
+    name = input("Please provide your name: ").upper()
+    #name = "test"
+    name_letters = list(name.upper())
+    try:
+        name_alfa = [alfabet_df_dict[letter] for letter in name]
+    except KeyError:
+        print("Please input a valid input.  Text only please.")
+        generate_phonetic()
+    else:
+        print("Your translation is: ")
+        print(name_alfa)
+        invalid_input = False
 
-print("Your translation is: ")
-print(name_alfa)
+generate_phonetic()
